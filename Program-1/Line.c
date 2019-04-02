55e925093f8399b0bde254e885cc22e5ffb11c75
Mustafa Chowdhury <44533993+Mustafa-chow@users.noreply.github.com>
2019-04-02 17:04:16 -0400

    Add files via upload
    Printing current directory and pointer

---
+++ Program-1/Line.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#ifndef PATH_MAX
+#define PATH_MAX
+#endif
+
+int main(void)
+{
+	char mycwd[PATH_MAX];
+
+	if (getcwd(mycwd, PATH_MAX) == NULL)
+	{
+		perror("Failed to get current working irectory");
+		return 1;
+	}
+	printf("Current working directory : %s\n", mycwd);
+	return 0;
+}
